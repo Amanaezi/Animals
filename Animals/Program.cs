86b609b14a38b4523cc740e9539efbfd8647dc7f
Artem <91727914+Amanaezi@users.noreply.github.com>
2023-01-28 10:35:25 +0200

    some fixes

--- Animals/Program.cs
+++ Animals/Program.cs
@@ -17,16 +17,16 @@ namespace Animals
 
             List<Animal> animals = new List<Animal>()
             {
-                new Predator() {Name = "Wolf", LengthWay = rnd.Next(0, 500), Weight = rnd.Next(10, 110)},
-                new Predator() {Name = "Racoon", LengthWay = rnd.Next(0, 500), Weight = rnd.Next(3, 9)},
-                new Vegetarian() {Name = "Cow", LengthWay = rnd.Next(0, 500), Weight = rnd.Next(700, 1100)},
-                new Omnivores(){Name = "Boar", LengthWay = rnd.Next(0, 500), Weight = rnd.Next(60, 100)},
-                new Omnivores(){Name = "Fox", LengthWay = rnd.Next(0, 500), Weight = rnd.Next(6, 10)},
-                new Vegetarian(){Name = "Deer", LengthWay = rnd.Next(0, 500), Weight = rnd.Next(150, 250)},
-                new Omnivores(){Name = "Bear", LengthWay = rnd.Next(0, 500), Weight = rnd.Next(8, 600)},
-                new Predator(){Name = "Badger", LengthWay = rnd.Next(0, 500), Weight = rnd.Next(10, 15)},
-                new Predator(){Name = "Lynx", LengthWay = rnd.Next(0, 500), Weight = rnd.Next(8, 30)},
-                new Vegetarian(){Name = "Beaver", LengthWay = rnd.Next(0, 500), Weight = rnd.Next(10, 30)}
+                new Predator() {Name = "Wolf", LengthWay = rnd.Next(1, 500), Weight = rnd.Next(10, 110)},
+                new Predator() {Name = "Racoon", LengthWay = rnd.Next(1, 500), Weight = rnd.Next(3, 9)},
+                new Vegetarian() {Name = "Cow", LengthWay = rnd.Next(1, 500), Weight = rnd.Next(700, 1100)},
+                new Omnivores(){Name = "Boar", LengthWay = rnd.Next(1, 500), Weight = rnd.Next(60, 100)},
+                new Omnivores(){Name = "Fox", LengthWay = rnd.Next(1, 500), Weight = rnd.Next(6, 10)},
+                new Vegetarian(){Name = "Deer", LengthWay = rnd.Next(1, 500), Weight = rnd.Next(150, 250)},
+                new Omnivores(){Name = "Bear", LengthWay = rnd.Next(1, 500), Weight = rnd.Next(8, 600)},
+                new Predator(){Name = "Badger", LengthWay = rnd.Next(1, 500), Weight = rnd.Next(10, 15)},
+                new Predator(){Name = "Lynx", LengthWay = rnd.Next(1, 500), Weight = rnd.Next(8, 30)},
+                new Vegetarian(){Name = "Beaver", LengthWay = rnd.Next(1, 500), Weight = rnd.Next(10, 30)}
             };
 
             foreach (var animal in animals)
